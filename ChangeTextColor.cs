06a002462d11787646a0eb55019175642992dddf
JacobFrye091 <vasilescuandrei945@gmail.com>
2022-10-12 15:36:32 +0300

    EAN13Class

---
+++ ChangeTextColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WinFormsApp1
+{
+    class ChangeTextColor
+    {
+        public static void ChangeColor(RichTextBox rtx,int Start,byte Length=1)
+        {
+            rtx.SelectionAlignment = HorizontalAlignment.Left;
+            rtx.SelectionStart = Start;
+            rtx.SelectionLength = Length;
+            rtx.SelectionColor = Color.Crimson;
+
+        }
+    }
+}
